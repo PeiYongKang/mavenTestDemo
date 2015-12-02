03bb51a34138034a86a24c3587256490ac321bd4
pi <15038183620@163.com>
2015-12-02 10:51:42 +0800

    test update2

--- src/main/java/com/test/Test2.java
+++ src/main/java/com/test/Test2.java
@@ -4,4 +4,13 @@ package com.test;
  * Created by peiyongkang on 2015/12/2.
  */
 public class Test2 {
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
